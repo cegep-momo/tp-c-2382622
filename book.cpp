5d2b3df2fc6654a236cecb1a011d72fc93835e1a
2382622 <2382622@cmontmorency.qc.ca>
2025-10-24 14:32:56 +0000

    [ADD]: book.cpp - Ajout des retours pour les getters et setters de chaque attribut

--- book.cpp
+++ book.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
-// Constructor
+// Constructor - livre disonible sans emprunteur
 Book::Book(const string& title, const string& author, const string& isbn) 
     : title(title), author(author), isbn(isbn), isAvailable(true), borrowerName("") {}
+
+// --- Getters ---
+
+// Retourne le titre du livre
+string Book::getTitle() const { return title; }
+
+// Retourne le nom de l’auteur
+string Book::getAuthor() const { return author; }
+
+// Retourne SBN du livre
+string Book::getISBN() const { return isbn; }
+
+// Indique si le livre est disponible (true = disponible, false = emprunté)
+bool Book::getAvailability() const { return isAvailable; }
+
+// Retourne le nom de l’emprunteur actuel (vide si disponible)
+string Book::getBorrowerName() const { return borrowerName; }
+
+// --- Setters ---
+
+// Modifie le titre du livre
+void Book::setTitle(const string& _title) { title = _title; }
+
+// Modifie l’auteur du livre
+void Book::setAuthor(const string& _author) { author = _author; }
+
+// Modifie ISBN
+void Book::setISBN(const string& _isbn) { isbn = _isbn; }
+
+// Modifie la disponibilité du livre  - disponible = pas d’emprunteur
+void Book::setAvailability(bool _available) { 
+    isAvailable = _available; 
+    if (_available) borrowerName.clear();
+}
+
+// Définit le nom de l’emprunteur - empreunteur =  livre non disponible
+void Book::setBorrowerName(const string& name) { 
+    borrowerName = name; 
+    if (!name.empty()) isAvailable = false;
+}
